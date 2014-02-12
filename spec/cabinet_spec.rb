8b2bbb492ab116e046ec47c8974f098297aeaa43
Taylor Price <tprice@onehealth.com>
2014-02-12 10:57:17 -0800

    setting up tests

--- spec/cabinet_spec.rb
+++ spec/cabinet_spec.rb
@@ -1,12 +1,22 @@
 # cabinet_spec.rb
-require "kitchen-cabinet"
+require File.expand_path('../../lib/kitchen-cabinet/cabinet.rb', __FILE__)
 describe Cabinet do
+  cookbook_name = "rspec-test-cookbook"
+  options = {}
+  options[:path] = 'test-path'
+  options[:copywrite] = "test-copywrite"
+  options[:license] = "test-license"
+  options[:email] = "test-email"
+  options[:help] = "test-help"
   describe "#self.init_service" do
-    it "returns 0 when successful" do
-      cabinet = Cabinet.new
+    it "Initialize all our services" do
+      puts 'init pass'
     end
   end
   
   describe "#self.write_configs" do 
+    it "writes configs" do
+      puts "config pass"
+    end
   end
 end
