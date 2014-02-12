3d98a51123234df5777d632a5a84a18efae61734
Taylor Price <tprice@onehealth.com>
2014-02-11 16:33:57 -0800

    still trying to fix path

--- lib/ohmeez/ohmeez.rb
+++ lib/ohmeez/ohmeez.rb
@@ -69,19 +69,19 @@ class Ohmeez
       if template == "chefspec"
         @spec.each do |spec|
           spec_path = File.join(path, 'spec')
-          tname = File.read("chefspec/#{spec}.eruby")
+          tname = File.read(File.read(File.join(File.dirname(File.expand_path(__FILE__)), "chefspec/#{spec}.eruby"))
           eruby = Erubis::Eruby.new(tname)
           File.open(File.join(spec_path, "#{spec}"), 'w') { |f| f.write(eruby.result(:cookbook_name=>cookbook_name)) }
         end
       elsif template == "serverspec"
         @spec.each do |spec|
           spec_path = File.join(path, 'spec')
-          tname = File.read("serverspec/#{spec}.eruby")
+          tname = File.read(tname = File.read(File.join(File.dirname(File.expand_path(__FILE__)), "serverspec/#{spec}.eruby"))
           eruby = Erubis::Eruby.new(tname)
           File.open(File.join(spec_path, "#{spec}"), 'w') { |f| f.write(eruby.result(:cookbook_name=>cookbook_name)) }
         end
       else
-        tname = File.read("templates/#{template}.eruby")
+        tname = File.read(File.join(File.dirname(File.expand_path(__FILE__)), "templates/#{template}.eruby"))
         eruby = Erubis::Eruby.new(tname)
         File.open(File.join(path, "#{template}"), 'w') { |f| f.write(eruby.result(:cookbook_name=>cookbook_name)) }
       end
